25686a3582465ec623840827f9427b09b0f68322
Gauthier Duffier <gauthier.duffier@efrei.net>
2023-11-13 00:27:55 +0100

    Add Spotify player to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { BsPlayFill } from 'react-icons/bs/';
+import { GrPauseFill } from 'react-icons/gr'
+import { IconContext } from "react-icons";
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [PlayerSpotify, setPlayerSpotify] = useState()
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  useEffect(() => {
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const token =
+        "BQC7aj8j2BOVspDDhuprCBvT1YBQb7pG8AbCl1P5mpqjqyNph0zjatWljaXar-e5Fo4hcv6Psu2YaHmzgnB-YO218YZ7EwMUocrfTposVuReuP2O3CXwMlPOlJMqp6ngYE3MD4oFCg7oOFkRBf2m0vwIOZNN6_VS0VFlSWfkd21UgvpdkYntY1XXvXA_cMtYra9FM01GGv9c4SGYo3XT0aWZ5809";
+      const player = new Spotify.Player({
+        name: "Web Playback SDK Quick Start Player",
+        getOAuthToken: (cb) => {
+          cb(token);
+        },
+        volume: 0.5,
+      });
+      setPlayerSpotify(player)
+      player.connect();
+      player.addListener("ready", ({ device_id }) => {
+        console.log("Ready with Device ID", device_id);
+      });
+    };
+  }, [])
+  
+
+  function play() {
+    if (isPlaying) {
+      setIsPlaying(false)
+    }
+    else {
+      setIsPlaying(true)
+    }
+    console.log("play");
+    PlayerSpotify.togglePlay();
+  }
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={play}>
+
+          {isPlaying ? <IconContext.Provider value={{ color: "white"}}><GrPauseFill /></IconContext.Provider> : <BsPlayFill />}
+
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        </div>
     </>
   )
 }
